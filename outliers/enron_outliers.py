29887debf5be8608b63f8a8f3b305c1598e960fa
vo0doo <kirsanov.bvt@gmail.com>
2021-02-25 23:40:25 +0300

    рефакторинг пути к импортированным библиотекам и перевод комментариев

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -4,16 +4,16 @@ import pickle
 import sys
 import matplotlib.pyplot
 sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
+from tools.feature_format import featureFormat, targetFeatureSplit
 
 
-### read in data dictionary, convert to numpy array
+### прочитать в словаре данных, преобразовать в массив numpy
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
-### your code below
+### ваш код ниже
 
 
 
