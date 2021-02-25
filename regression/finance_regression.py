367e02b4055f69d3b94751b6ab8160eb58fb6c6b
vo0doo <kirsanov.bvt@gmail.com>
2021-02-25 14:32:41 +0300

    переведены коментарии

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -1,65 +1,64 @@
 #!/usr/bin/python
 
 """
-    Starter code for the regression mini-project.
-    
-    Loads up/formats a modified version of the dataset
-    (why modified?  we've removed some trouble points
-    that you'll find yourself in the outliers mini-project).
+    Стартовый код для мини-проекта регрессии.
 
-    Draws a little scatterplot of the training/testing data
+    Загружает / форматирует измененную версию набора данных
+    (зачем модифицировать? мы удалили некоторые проблемы
+    что вы попадете в мини-проект выбросов).
 
-    You fill in the regression code where indicated:
+    Рисует небольшую диаграмму рассеяния данных обучения / тестирования
+
+    Вы вводите код регрессии в указанном месте:
 """    
 
 
 import sys
 import pickle
 sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
+from tools.feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
-### list the features you want to look at--first item in the 
-### list will be the "target" feature
+### перечислите функции, на которые вы хотите обратить внимание - первый элемент в
+### список будет "целевой" функцией
 features_list = ["bonus", "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
-### training-testing split needed in regression, just like classification
+### разделение обучения и тестирования необходимо в регрессии, как и при классификации
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
-### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
-### plots it correctly. Don't forget to change the test_color above from "b" to
-### "r" to differentiate training points from test points.
+### Ваш регресс идет сюда!
+### Пожалуйста, назовите его reg, чтобы приведенный ниже код построения подобрал его и
+### правильно рисует. Не забудьте изменить test_color выше с "b" на
+### «r», чтобы отличать тренировочные точки от контрольных.
+from sklearn.
 
 
 
 
 
 
-
-
-### draw the scatterplot, with color-coded training and testing points
+### нарисуйте диаграмму рассеяния с цветными обозначениями точек обучения и тестирования
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
     plt.scatter( feature, target, color=test_color ) 
 for feature, target in zip(feature_train, target_train):
     plt.scatter( feature, target, color=train_color ) 
 
-### labels for the legend
+### этикетки для легенды
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
 
 
 
-### draw the regression line, once it's coded
+### нарисуйте линию регрессии, как только она закодирована
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
