f2ea4b46b0580fd7e04a8e314dcb0ec2de3f5a80
vo0doo <kirsanov.bvt@gmail.com>
2021-02-25 23:35:50 +0300

    перевод коментариев и текста внутри функции print, рефакторинг функции print в функцию

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -5,26 +5,26 @@ import numpy
 import matplotlib.pyplot as plt
 import pickle
 
-from outlier_cleaner import outlierCleaner
+from outliers.outlier_cleaner import outlierCleaner
 
 
-### load up some practice data with outliers in it
+### загрузить некоторые данные практики с выбросами в нем
 ages = pickle.load( open("practice_outliers_ages.pkl", "r") )
 net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "r") )
 
 
 
-### ages and net_worths need to be reshaped into 2D numpy arrays
-### second argument of reshape command is a tuple of integers: (n_rows, n_columns)
-### by convention, n_rows is the number of data points
-### and n_columns is the number of features
+### age и net_worths необходимо преобразовать в двумерные массивы numpy
+### второй аргумент команды reshape - кортеж целых чисел: (n_rows, n_columns)
+### по соглашению n_rows - это количество точек данных
+### и n_columns - количество функций
 ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
 net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1, random_state=42)
 
-### fill in a regression here!  Name the regression object reg so that
-### the plotting code below works, and you can see what your regression looks like
+### заполните регрессию здесь! Назовите объект регрессии reg, чтобы
+### приведенный ниже код построения графика работает, и вы можете увидеть, как выглядит ваша регрессия
 
 
 
@@ -44,35 +44,29 @@ plt.scatter(ages, net_worths)
 plt.show()
 
 
-### identify and remove the most outlier-y points
+### выявить и удалить наиболее резко выделяющиеся точки
 cleaned_data = []
 try:
     predictions = reg.predict(ages_train)
     cleaned_data = outlierCleaner( predictions, ages_train, net_worths_train )
 except NameError:
-    print "your regression object doesn't exist, or isn't name reg"
-    print "can't make predictions to use in identifying outliers"
+    print("ваш объект регрессии не существует или не имеет имени reg")
+    print("не может делать прогнозы для использования при выявлении выбросов")
 
-
-
-
-
-
-
-### only run this code if cleaned_data is returning data
+### запускайте этот код только в том случае, если cleaned_data возвращает данные
 if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
     ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
     net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
 
-    ### refit your cleaned data!
+    ### восстановить ваши очищенные данные!
     try:
         reg.fit(ages, net_worths)
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
-        print "you don't seem to have regression imported/created,"
-        print "   or else your regression object isn't named reg"
-        print "   either way, only draw the scatter plot of the cleaned data"
+        print("похоже, что регрессия не импортирована / не создана,")
+        print("иначе ваш объект регрессии не назван reg")
+        print("в любом случае, нарисуйте только диаграмму рассеяния очищенных данных")
     plt.scatter(ages, net_worths)
     plt.xlabel("ages")
     plt.ylabel("net worths")
@@ -80,5 +74,5 @@ if len(cleaned_data) > 0:
 
 
 else:
-    print "outlierCleaner() is returning an empty list, no refitting to be done"
+    print("outlierCleaner() возвращает пустой список, переустановка не требуется")
 
