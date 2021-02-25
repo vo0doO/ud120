38f8c4fcbd4b478f261399842713f228518ff8fb
vo0doo <kirsanov.bvt@gmail.com>
2021-02-25 23:37:03 +0300

    измененение настроек под python3

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -17,16 +17,16 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from tools.feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified_dos.pkl", "rb") )
 
 ### перечислите функции, на которые вы хотите обратить внимание - первый элемент в
 ### список будет "целевой" функцией
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys_dos.pkl')
 target, features = targetFeatureSplit( data )
 
 ### разделение обучения и тестирования необходимо в регрессии, как и при классификации
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
 test_color = "r"
@@ -37,9 +37,9 @@ test_color = "r"
 ### Пожалуйста, назовите его reg, чтобы приведенный ниже код построения подобрал его и
 ### правильно рисует. Не забудьте изменить test_color выше с "b" на
 ### «r», чтобы отличать тренировочные точки от контрольных.
-from sklearn.
-
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
 
 
@@ -63,7 +63,10 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
+stop = "stop"
